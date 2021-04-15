4e66c57c80c7af42d20c650144b17b0b56344b58
Lilit <lilo22011998@gmail.com>
2021-04-15 15:58:55 +0400

    src: problem 228A.

--- src/228/228A.js
+++ src/228/228A.js
@@ -5,7 +5,7 @@
     var i;
     var result = 0;
     for (i = 0; i < items.length - 1; i++) {
-        if (items[i] === items[i + 1] || items[i] === items[i + 2] ||items[i] === items[i + 3] || items[i] ===items[i + 5]) {
+        if (items[i] === items[i + 1] || items[i] === items[i + 2] || items[i] === items[i + 3] || items[i] === items[i + 5]) {
             result += 1;
         }
     }
