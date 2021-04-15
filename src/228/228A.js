c3f0553aa827bbb47aa1672100ee967a2f810089
Lilit <lilo22011998@gmail.com>
2021-04-15 15:58:40 +0400

    src: problem 228A.

---
+++ src/228/228A.js
@@ -0,0 +1,14 @@
+(function () {
+    const line = readline();
+    const items = line.split(' ');
+
+    var i;
+    var result = 0;
+    for (i = 0; i < items.length - 1; i++) {
+        if (items[i] === items[i + 1] || items[i] === items[i + 2] ||items[i] === items[i + 3] || items[i] ===items[i + 5]) {
+            result += 1;
+        }
+    }
+
+    print(result);
+})();
